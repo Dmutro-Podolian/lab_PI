ca5d13c1536c01cce4cd2442e5c528de3cf73673
Дмитро Подолян <118061088+Dmutro-Podolian@users.noreply.github.com>
2026-03-10 10:02:45 +0200

    Add files via upload

---
+++ lab1/Unittest.py
@@ -0,0 +1,20 @@
+# import unittest 
+# from lab1 import subarray_sort_range
+
+
+# class TestSort(unittest.TestCase):
+#     def test_sorted(self):
+#         arr = [1, 2, 3, 4, 5]
+#         self.assertEqual(subarray_sort_range(arr), (-1, -1))
+
+#     def test_full_unsorted(self):
+#         arr = [7, 6, 7, 2]
+#         self.assertEqual(subarray_sort_range(arr), (0, len(arr) - 1))
+
+#     def test_single_element(self):
+#         arr = [1]
+#         self.assertEqual(len(arr), 1)
+
+# if __name__ == '__main__':
+#     unittest.main()
+    
