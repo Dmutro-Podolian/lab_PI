f5d4d118bd5f192a8f5df8cbfd5b2f67b16cc8c9
Дмитро Подолян <118061088+Dmutro-Podolian@users.noreply.github.com>
2026-04-07 12:44:48 +0300

    Add files via upload

---
+++ lab3/Unittest.py
@@ -0,0 +1,15 @@
+# import unittest
+from lab3 import BinaryTree, binar_list
+
+root = BinaryTree(3)
+root.left = BinaryTree(9)
+root.right = BinaryTree(20)
+
+# class TestCheckResult(unittest.TestCase):
+#     # def test_example_1(self):
+#     #     self.assertTrue(is_tree_balanced(root))
+
+if __name__ == '__main__':
+    # unittest.main()
+    print(binar_list)
+
